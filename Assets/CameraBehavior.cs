6103851e37294c825a6508fd6a658562d7580b04
Alec Caputo <aleccaputo7@gmail.com>
2020-02-22 09:48:55 -0500

    mr dealy

---
+++ Assets/CameraBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Transform Transform;
+    public float TrackSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var myLocation = gameObject.transform.position;
+        var distance = Vector2.Distance(myLocation, Transform.position);
+        var newLocation = Vector2.MoveTowards(myLocation, Transform.position, TrackSpeed * Time.deltaTime * distance);
+        var newNewLocation = new Vector3(newLocation.x, newLocation.y, myLocation.z);
+        gameObject.transform.position = newNewLocation;
+    }
+}
---
+++ Assets/MrDealySwordBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrDealySwordBehavior : MonoBehaviour
+{
+    public float AttackForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        var targetLocation = collider.transform.position;
+        var collisionPoint = gameObject.transform.position;
+        var diff = (targetLocation - collisionPoint).normalized * AttackForce;
+
+        var rigidBody = collider.gameObject.GetComponent<Rigidbody2D>();
+        rigidBody.AddForce(diff);
+    }
+}
